3552cef161da5dd62f48069039abe596cd534afb
EliasGabriel1 <elias.14782'@hotmail.com>
2022-06-16 14:10:17 -0300

    update

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 function Temporizador(initiate) {
     if (initiate !== true) {
-        $("#first").toggleClass("none");
-        $("#second").toggleClass("block");
+        $(".first").toggleClass("none");
+        $(".second").toggleClass("block");
     }
-    setTimeout(Temporizador, 5000);
+    setTimeout(Temporizador, 8000);
+}
+
+function Temporizador2(initiate) {
+    if (initiate !== true) {
+        $(".first2").toggleClass("none");
+        $(".second2").toggleClass("block");
+    }
+    setTimeout(Temporizador2, 4000);
 }
 
 $(function () {
+    Temporizador2(true);
     Temporizador(true);
 });
